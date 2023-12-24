835d8ed205ca8a54ca24c01130db3b80bf209db2
badrkacimi <badrvkacimi@gmail.com>
2023-12-24 22:31:44 +0100

    Initial commit

---
+++ src/test/java/com/stayUpdated/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayUpdated.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
